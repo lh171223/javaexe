b9fea37e9e95a7f0f0a4e053772fc93c342d9b42
lh171223 <1532589995@qq.com>
2019-11-08 08:57:19 +0800

    20191107

--- src/offer/Main.java
+++ src/offer/Main.java
@@ -1,25 +1,53 @@
 package offer;
-
+/*
+中位数：排序后数组的中间值。若是奇数个：最中间的那个数；若是偶数个：最中间两个数的平均数
+ */
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Scanner;
-//https://www.nowcoder.com/practice/47232470945644458213ddd07580e121
 public class Main{
     public static void main(String[] args){
         //中位数
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();//初始数组大小
         int x = sc.nextInt();//新数组的median数
-        int[] arr = new int[n];
+        ArrayList<Integer> arr = new ArrayList<>();
         for (int i=0;i<n;i++)
-            arr[i] = sc.nextInt();
-        int subscript =0;
+            arr.add(sc.nextInt());
+        Collections.sort(arr);
+        for (int ele:arr
+             ) {
+            System.out.print(ele);
+            System.out.print(" ");
+        }
+        int sub;
+        if (arr.contains(x)){
         for (int i=0;i<n;i++){
-            if (arr[i] == x){
-                subscript = i;
+            if (x==arr.get(i)){
+                sub=i;
+                if (n-1-sub>sub){
+                    System.out.println(n-1-sub-1);
+                }else
+                    System.out.println(n-1-sub);
                 break;
             }
         }
-        int number=0;
-        number = subscript*2-n;
-        System.out.print(number);
+        }else
+            if (count(arr,x) == n)
+                System.out.print(n+1);
+            else
+                System.out.print(arr.size() - count(arr,x) - count(arr,x));
+
     }
+
+    private static int count(ArrayList<Integer> a,int x){
+        //计算比x小的数的个数
+        for (int i=0;i<a.size();i++){
+            if (a.get(i) >x ){
+                return i;
+            }
+        }
+        return a.size();
+    }
+
 }
